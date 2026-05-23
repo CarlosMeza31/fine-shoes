4433125c3cecba313d4c42beb9486e72e5731918
Carlos Iriarte <carlos@gmail.com>
2026-05-23 08:38:59 -0700

    Permitir todos los origenes en CORS

--- backend/app.py
+++ backend/app.py
@@ -40,7 +40,7 @@ app = FastAPI(
 # ------------------------------------------------------------
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[settings.FRONTEND_URL, "http://127.0.0.1:5500"],
+    allow_origins=["*"],
     allow_credentials=True,     # necesario para enviar cookies/headers de auth
     allow_methods=["*"],        # GET, POST, PUT, DELETE...
     allow_headers=["*"],        # Authorization, Content-Type...
